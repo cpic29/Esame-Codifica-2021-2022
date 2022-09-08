2abf495fe745a4ee0cab1d9c7a9bc96ae01176de
cpic29 <112935547+cpic29@users.noreply.github.com>
2022-09-08 10:22:28 +0200

    Add files via upload

---
+++ ProgettoCodifica/javascript.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    $('.trad').hover(function(){
+        $t1 = $(this).attr('id');
+        $($t1).toggleClass("ev");
+        $(this).toggleClass("ev");
+    });
+
+    $('#retro').click(function(){
+        $('#19').toggleClass('u-hide');
+        $('#19R').toggleClass('u-hide');
+        $('#19R').toggleClass('u-max-full-width');
+    });
+
+    $('#change').click(function(){
+        $('#p1').toggleClass('u-hide');
+        $('#p2').toggleClass('u-hide');
+        if($("#change").val() == 'Pagina 19'){
+            $("#change").val('Pagina 20');
+        } else {
+            $("#change").val('Pagina 19');
+        }
+    });
+
+    $('#19').click(function(){
+        $('#p1').removeClass('u-hide');
+        $('#p2').addClass('u-hide');
+        $("#change").val('Pagina 20');
+    });
+
+    $('#20').click(function(){
+        $('#p2').removeClass('u-hide');
+        $('#p1').addClass('u-hide');
+        $("#change").val('Pagina 19');
+    });
+
+    $('#del').click(function(){
+    $("#del").toggleClass("v");
+    $(".del").toggleClass( "u-hide v" );
+    });
+
+    $('#abbr').click(function(){
+        $("#abbr").toggleClass("a");
+        $(".abbr").toggleClass( "u-hide a" );
+        $(".expan").toggleClass( "u-hide" )
+    });
+
+    $('#for').click(function(){
+        $("#for").toggleClass("c");
+        $(".for").toggleClass( "c" );
+    });
+
+    $('#note').click(function(){
+        $("#note").toggleClass("g");
+        $(".note").toggleClass( "u-hide g" );
+    });
+
+    $('#glot').click(function(){
+        $('#glot').toggleClass("bl");
+        $('.glot').toggleClass("bl");
+    });
+
+    $('#term').click(function(){
+        $("#term").toggleClass("r");
+        $(".term").toggleClass( "r" );
+    });
+
+    $(".term").hover(function(){
+        $(this).toggleClass( "r" );
+    });
+
+    $(".term").click(function(){
+        $t1 = $(this).attr('id');
+        $($t1).addClass('r');
+
+        setTimeout(function() {
+            $($t1).removeClass('r');
+        }, 6000);
+    });
+
+
+});
